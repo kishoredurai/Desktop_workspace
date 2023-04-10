0b385d951e459cc1434c87fecdb37ebb689b5736
kishoredurai <kishore.ct19@bitsathy.ac.in>
2023-04-10 13:40:34 +0530

    logo changed

--- client/src/components/pages/admin/admin_dashboard.js
+++ client/src/components/pages/admin/admin_dashboard.js
@@ -135,7 +135,7 @@ const renderDatas = () => {
                           {data.status}
                         </div>
                       </td>
-                      <td className="py-4 px-6 border-r">{data.category}</td>
+                      <td className="py-4 px-6 border-r">{data.createdate}</td>
 
                       <td className="py-4 px-6">
                       <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
